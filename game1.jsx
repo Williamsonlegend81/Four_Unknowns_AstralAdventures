54f8df8b7653331ac76314e8494900ff1899bbdd
Vrund Kansara <159721235+Williamsonlegend81@users.noreply.github.com>
2024-08-10 17:24:29 +0530

    Create game1.jsx

---
+++ game1.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+
+const game1 = () => {
+  return (
+    <div>
+      <section class="memory-game">
+    <div class="memory-card" data-framework="aurelia">
+      <img class="front-face" src="Scientists/Homi_bhabha.jpg" alt="Aurelia" />
+      <img class="back-face" src="Scientists/Back.png" alt="Back_of_card" />
+    </div>
+    <div class="memory-card" data-framework="aurelia">
+        <h1 class="front-face">Homi J Bhabha</h1>
+      <img class="back-face" src="Scientists/Back.png" alt="Back_of_card" />
+    </div>
+
+    <div class="memory-card" data-framework="vue">
+      <img class="front-face" src="Scientists/Kalpana_chavla.jpg" alt="Vue" />
+      <img class="back-face" src="Scientists/Back.png" alt="Back_of_card" />
+    </div>
+    <div class="memory-card" data-framework="vue">
+        <h1 class="front-face">Kalpana Chavla</h1>
+      <img class="back-face" src="Scientists/Back.png" alt="Back_of_card" />
+    </div>
+
+    <div class="memory-card" data-framework="angular">
+      <img class="front-face" src="Scientists/cv_raman.jpg" alt="Angular" />
+      <img class="back-face" src="Scientists/Back.png" alt="Back_of_card" />
+    </div>
+    <div class="memory-card" data-framework="angular">
+       <h1 class="front-face">CV RAMAN</h1>
+      <img class="back-face" src="Scientists/Back.png" alt="Back_of_card" />
+    </div>
+
+    <div class="memory-card" data-framework="ember">
+      <img class="front-face" src="Scientists/Rakesh_sharma_edit.jpg" alt="Ember" />
+      <img class="back-face" src="Scientists/Back.png" alt="Back_of_card" />
+    </div>
+    <div class="memory-card" data-framework="ember">
+        <h1 class="front-face">Rakesh Sharma</h1>
+      <img class="back-face" src="Scientists/Back.png" alt="Back_of_card" />
+    </div>
+
+    <div class="memory-card" data-framework="backbone">
+      <img class="front-face" src="Scientists\A._P._J._Abdul_Kalam.jpg" alt="Backbone" />
+      <img class="back-face" src="Scientists/Back.png" alt="Back_of_card" />
+    </div>
+    <div class="memory-card" data-framework="backbone">
+        <h1 class="front-face">Dr. APJ Abdul Kalam</h1>
+      <img class="back-face" src="Scientists/Back.png" alt="Back_of_card" />
+    </div>
+
+    <div class="memory-card" data-framework="react">
+      <img class="front-face" src="Scientists/JC_Bose.jpg" alt="React" />
+      <img class="back-face" src="Scientists/Back.png" alt="Back_of_card" />
+    </div>
+    <div class="memory-card" data-framework="react">
+        <h1 class="front-face">J C Bose</h1>
+      <img class="back-face" src="Scientists/Back.png" alt="Back_of_card" />
+    </div>
+  </section>
+    </div>
+  )
+}
+
+export default game1
