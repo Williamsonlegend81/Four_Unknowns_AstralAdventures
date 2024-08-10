1e2cd123e63c009b265d3fe41bafc518f43bcb00
Vrund Kansara <159721235+Williamsonlegend81@users.noreply.github.com>
2024-08-10 17:07:03 +0530

    Create universe.jsx

---
+++ universe.jsx
@@ -0,0 +1,13 @@
+// universe.jsx
+import React from 'react';
+
+const universe = () => {
+  return (
+    <div>
+      <h1>Our Universe</h1>
+      <p>Explore the vast universe and its mysteries...</p>
+    </div>
+  );
+};
+
+export default universe;
