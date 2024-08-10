5b5baa1cebfd7ed854f3aef5924f6f8ed7a91776
Vrund Kansara <159721235+Williamsonlegend81@users.noreply.github.com>
2024-08-10 17:06:47 +0530

    Create scientist.jsx

---
+++ scientist.jsx
@@ -0,0 +1,13 @@
+// scientist.jsx
+import React from 'react';
+
+const Scientist = () => {
+  return (
+    <div>
+      <h1>Indian Scientists</h1>
+      <p>Learn about the great Indian scientists and their contributions...</p>
+    </div>
+  );
+};
+
+export default Scientist;
